6e8a4852e07e4ff44d1e554b284da8d994aa8265
MSI_notebook <dushiun@gmail.com>
2020-10-16 23:35:55 +0800

    "update 2335"

--- HW2.py
+++ HW2.py
@@ -21,16 +21,17 @@ def training(trainx,trainy,weight,learningrate,epoch):
             newtrainy = trainy[per]
 
         for single_trainx,single_trainy in zip(newtrainx,newtrainy):
-            if (  sign(np.dot(weight,single_trainx) )!=sign(single_trainy)):
+            if (  sign(np.dot(weight_list[-1],single_trainx) )!=sign(single_trainy)):
                 print(weight_list[-1])
                 weight_list.append(
                     weight_update(weight=weight_list[-1],trainy=single_trainy,learningrate=learningrate,trainx=single_trainx)   )
                 print('pass{}'.format(passnum))
                 # os.system('pause')
                 break
-            passnum+=1
+            else:
+                passnum+=1
 
-        if passnum==7:
+        if passnum==8:
             break
 
         passnum=0
@@ -44,9 +45,9 @@ if __name__ == '__main__':
     x=trainx[:,0]
     y=trainx[:,1]
     weight=np.array([1,-1,0.2])
+    # weight = np.array([-1, -1, 1.2])
 
-
-    weight_list=training(trainx=trainx,trainy=trainy,weight=weight,learningrate=0.15,epoch=35)
+    weight_list=training(trainx=trainx,trainy=trainy,weight=weight,learningrate=0.8,epoch=25)
 
     f = lambda x: (weight[0]/-weight[1])*x+weight[2]
     graphx = np.array([-100, 100])
@@ -59,13 +60,13 @@ if __name__ == '__main__':
         print(weight)
         if index != len(weight_list) - 1:
             pass
-            # plt.plot(graphx, f(graphx), c="red", label='line' + str(index))
+            # plt.plot(graphx, f(graphx), c="green", label='line' + str(index))
         else:
             plt.plot(graphx,f(graphx), c="orange", label='line'+str(index))
 
-
+    plt.legend(scatterpoints=1, markerscale=0.2)
     plt.scatter(x, y, alpha=0.6, label = 'data')
-    plt.xlim(-1.2, 1.2)
+    plt.xlim(0, 1.2)
     plt.ylim(0, 1.2)
-    plt.legend()
+
     plt.show()
