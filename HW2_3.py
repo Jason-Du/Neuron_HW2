0e79dab9e76aba63d43364176efb3c6d90fc81f2
“Jason-Du(NCKU) <53274895+Jason-Du@users.noreply.github.com>
2020-10-17 16:29:42 +0800

    "update 2_3"

---
+++ HW2_3.py
@@ -0,0 +1,20 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import copy
+import os
+def weight_update(weight,trainx,trainy):
+    upadte_weight = weight+0.5*(np.multiply(trainy,trainx))
+    return upadte_weight
+
+if __name__ == '__main__':
+    pass
+    x=np.array([[2,-0.5,1],[2,0.5,2]])
+    y=np.array([[3,0.5,1],[3,-0.5,2]])
+    weight=np.array([1,-2,0.5])
+    update_list=[weight]
+    for singlex,singley in zip(x,y):
+        print(singlex)
+        print(singley)
+        print(update_list[-1])
+        update_list.append(weight_update(weight=update_list[-1],trainx=singlex,trainy=singley))
+        print(update_list[-1])
