a3ee1ce8d7ade034204ee8be3c705b42866e37ad
“Jason-Du(NCKU) <53274895+Jason-Du@users.noreply.github.com>
2020-10-17 16:03:16 +0800

    "update 2_1_3"

--- HW2.py
+++ HW2.py
@@ -1,13 +1,15 @@
 import numpy as np
 from numpy import *
 import matplotlib.pyplot as plt
+import copy
 import os
 def weight_update(weight,trainy,learningrate,trainx):
     upadte_weight = weight + (trainy * learningrate *trainx)
     return upadte_weight
-# def weight_update2(weight,trainy,learningrate,trainx):
-#     upadte_weight = weight + (trainy * learningrate *trainx)
-#     return upadte_weight
+def weight_update2(weight,trainy,learningrate,trainx):
+    upadte_weight= weight + (trainy * learningrate *trainx)
+    upadte_weight[1]=copy.deepcopy(weight[1])
+    return upadte_weight
 def training(trainx,trainy,weight,learningrate,epoch):
     weight_list=[weight]
     passnum=0
@@ -22,7 +24,40 @@ def training(trainx,trainy,weight,learningrate,epoch):
         for single_trainx,single_trainy in zip(newtrainx,newtrainy):
             if (  sign(np.dot(weight_list[-1],single_trainx) )!=sign(single_trainy)):
                 weight_list.append(
-                    weight_update(weight=weight_list[-1],trainy=single_trainy,learningrate=learningrate,trainx=single_trainx)   )
+                weight_update(weight=weight_list[-1],trainy=single_trainy,learningrate=learningrate,trainx=single_trainx)   )
+
+                break
+            else:
+                passnum+=1
+        if passnum==8:
+            print(i + 1)
+            # print('jumppass{}'.format(passnum))
+            break
+
+        passnum = 0
+    return weight_list
+def training2(trainx,trainy,weight,learningrate,epoch):
+    weight_list=[weight]
+    passnum=0
+    for i in range(epoch):
+        passnum = 0
+        per = np.random.permutation(trainx.shape[0])
+        # 打亂後的行號
+        newtrainx = trainx[per, :]
+        # 獲取打亂後的訓練資料
+        newtrainy = trainy[per]
+
+        for single_trainx,single_trainy in zip(newtrainx,newtrainy):
+            # copy_single_trainx = copy.deepcopy(single_trainx)
+            # copy_weight=copy.deepcopy(weight_list[-1])
+            # del copy_single_trainx[1]
+            # del copy_weight[1]
+            copy_single_trainx = np.delete(single_trainx, 1)
+            copy_weight = np.delete(weight_list[-1], 1)
+            if (  sign(np.dot(copy_weight,copy_single_trainx) )!=sign(single_trainy)):
+                weight_list.append(
+                weight_update2(weight=weight_list[-1],trainy=single_trainy,learningrate=learningrate,trainx=single_trainx)   )
+
                 break
             else:
                 passnum+=1
@@ -43,19 +78,37 @@ if __name__ == '__main__':
     y=trainx[:,1]
     weight=np.array([1,-1,0.2])
     weight_list=training(trainx=trainx,trainy=trainy,weight=weight,learningrate=0.8,epoch=30)
-    print(weight_list)
+    weight_list2=training2(trainx=trainx,trainy=trainy,weight=weight,learningrate=0.8,epoch=30)
     f = lambda x: (weight[0]/-weight[1])*x-(weight[2]/weight[1])
-    # print(len(weight_list))
+    print(len(weight_list))
+    print(len(weight_list2))
+    plt.figure(1)
     graphx = np.array([-100, 100])
-    plt.plot(graphx, f(graphx), c="red", label='line1')
+    plt.plot(graphx, f(graphx), c="red", label='Initial')
     weight = weight_list[1]
     plt.plot(graphx, f(graphx), c="green", label='line2')
     weight=weight_list[-1]
     plt.plot(graphx,f(graphx), c="orange", label='lastline')
 
-    plt.legend(scatterpoints=1, markerscale=0.1)
+    plt.legend()
     plt.scatter(x, y, alpha=0.6, label = 'data',s=10, marker='o')
     plt.xlim(-0.5, 1.2)
     plt.ylim(-0.5, 1.2)
+    plt.figure(2)
+    for weight_index,single_weight in enumerate(weight_list2):
+        pass
+        weight=single_weight
+        if weight_index == len(weight_list) -1:
+            plt.plot(graphx,f(graphx), c="orange", label='lastline')
 
+        if weight_index == 0:
+            plt.plot(graphx, f(graphx), c="red", label='Initial')
+        else:
+            plt.plot(graphx, f(graphx), c="green", label='line'+str(weight_index))
+
+
+    plt.legend()
+    plt.scatter(x, y, alpha=0.6, label = 'data',s=10, marker='o')
+    plt.xlim(-0.5, 1.2)
+    plt.ylim(-0.5, 1.2)
     plt.show()
